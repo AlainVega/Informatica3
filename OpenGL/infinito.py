df2ad32d84f61d46b4262262a7a3f0f60d3b0f4a
Alain Vega <alainjosevz@gmail.com>
2023-10-23 17:26:46 -0300

    agregar relleno a infinito

--- OpenGL/infinito.py
+++ OpenGL/infinito.py
@@ -39,7 +39,7 @@ n = l -r -(3*a)/2
 vertical, horizontal = 0,0
 size = 1
 
-# Sirve para dibujar la cara del poligono (Hexagono)
+# Sirve para dibujar la cara del poligono. (Pintar el ancho del infinito)
 def cara(vertices, color):
     # Setear las propiedades del material.
     c = [color[0], color[1], color[2], 1]
@@ -51,7 +51,7 @@ def cara(vertices, color):
 
     glColor(color[0], color[1], color[2], 1) # pintar con este color
 
-    glBegin(GL_TRIANGLE_FAN) # dibuja triangulos para simular el hexagono.
+    glBegin(GL_TRIANGLE_STRIP) # dibuja triangulos para simular el hexagono.
     for vertice in vertices:
         glVertex3fv(vertice)
     glEnd() # fin del contexto de triangulos
@@ -72,27 +72,32 @@ def punto(vertices, color):
         glVertex3fv(vertice)
     glEnd() 
 
+# Denota los puntos de un arco de circunferencia en un plano
+# Retorna la lista de puntos.
 def arco(centro, radio, color):
+    v = []
     glColor(color[0], color[1], color[2], 1) # pintar con este color
     glBegin(GL_LINE_STRIP) 
     x, y = 0, 0
-    x0, y0, x1, y1 = 0,0,0,0
+    # x0, y0, x1, y1 = 0,0,0,0
     if centro[0] >= 0:
         for alpha in range(-90, 91):
             x = radio*math.cos(math.radians(alpha)) + centro[0]
             y = radio*math.sin(math.radians(alpha)) + centro[1]
             glVertex3f(x, y, 0)
-        x0, y0 = radio*math.cos(math.radians(-90)) + centro[0], radio*math.sin(math.radians(-90)) + centro[1]
-        x1, y1 = radio*math.cos(math.radians(90)) + centro[0], radio*math.sin(math.radians(90)) + centro[1]
+            v.append((x,y,0))
+        # x0, y0 = radio*math.cos(math.radians(-90)) + centro[0], radio*math.sin(math.radians(-90)) + centro[1]
+        # x1, y1 = radio*math.cos(math.radians(90)) + centro[0], radio*math.sin(math.radians(90)) + centro[1]
     else:
         for alpha in range(-90, 91):
             x = -radio*math.cos(math.radians(alpha)) + centro[0]
             y = -radio*math.sin(math.radians(alpha)) + centro[1]
             glVertex3f(x, y, 0)
-        x0, y0 = -radio*math.cos(math.radians(-90)) + centro[0], radio*math.sin(math.radians(-90)) + centro[1]
-        x1, y1 = -radio*math.cos(math.radians(90)) + centro[0], radio*math.sin(math.radians(90)) + centro[1]
+            v.append((x,y,0))
+        # x0, y0 = -radio*math.cos(math.radians(-90)) + centro[0], radio*math.sin(math.radians(-90)) + centro[1]
+        # x1, y1 = -radio*math.cos(math.radians(90)) + centro[0], radio*math.sin(math.radians(90)) + centro[1]
     glEnd()
-    return ((x0, y0, 0), (x1, y1, 0))
+    return v
     
 
 # Sirve para definir el infinito.
@@ -103,30 +108,54 @@ def infinito():
 
     # USANDO EL ALGORITMO DEL PINTOR 
     # (dibujar primero los objetos lejanos, los de atras)
-    vertices.append((0,0,0))
-    vertices.append((a/2,0,0))
-    vertices.append((0,a/2,0))
-    vertices.append((-a/2,0,0))
-    vertices.append((0,-a/2,0))
-
+    # Vertices en sentido antihorario
+    vertice0 = (0,0,0) # centro del infinito
+    vertice1 = (a/2,0,0) 
+    vertice2 = (0,-a/2,0)
+    vertice3 = (-a/2,0,0)
+    vertice4 = (0,a/2,0)
     # punto(vertices, (0.7, 0.2, 0))
 
     glTranslate(horizontal, vertical, 0)
     glScale(size, size, 0)
 
-    centro = (a/2 + n, 0)
-    puntos = arco(centro, r, (1, 1, 1))
-    recta([(a/2,0,0), puntos[0], (a/2,0,0), puntos[1]], (1, 1, 1))
+    centro = (a/2 + n, 0) # centro de las 2 semi circunferencias (centro de la semi dona derecha)
+    interior = arco(centro, r, (1, 1, 1)) # delimita el arco interior
+    recta([vertice1, interior[0], vertice1, interior[-1]], (1, 1, 1)) # dibuja el arco
     
-    puntos = arco(centro, r+a, (1, 1, 1))
-    recta([(0,-a/2,0), puntos[0], (0,a/2,0), puntos[1]], (1, 1, 1))
+    exterior = arco(centro, r+a, (1, 1, 1)) # delimita el arco exterior
+    recta([vertice2, exterior[0], vertice4, exterior[-1]], (1, 1, 1)) # dibuja el arco
 
-    centro = (-a/2 - n, 0)
-    puntos = arco(centro, r, (1, 1, 1))
-    recta([(-a/2,0,0), puntos[0], (-a/2,0,0), puntos[1]], (1, 1, 1))
+    # Pintamos la semi dona de la derecha
+    for i in range(len(interior)):
+        vertices.append(interior[i])
+        vertices.append(exterior[i])
+    cara(vertices, (1,1,1)) # pinta la semi dona
 
-    puntos = arco(centro, r+a, (1, 1, 1))
-    recta([(0,-a/2,0), puntos[0], (0,a/2,0), puntos[1]], (1, 1, 1))
+    vertices.clear()
+
+    cara([vertice1, vertice2, interior[0], exterior[0]], (1,1,1)) # Pintar lazo de arriba derecha
+    cara([vertice1, vertice4, interior[-1], exterior[-1]], (1,1,1)) # Pintar lazo de abajo derecha
+
+    centro = (-a/2 - n, 0) # centro de las 2 semi circunferencias (centro de la semi dona izquierda)
+    interior = arco(centro, r, (1, 1, 1)) # delimita el arco interior
+    recta([vertice3, interior[-1], vertice3, interior[0]], (1, 1, 1)) # dibuja el arco
+
+    exterior = arco(centro, r+a, (1, 1, 1)) # delimita el arco exterior
+    recta([vertice2, exterior[-1], vertice4, exterior[0]], (1, 1, 1)) # dibuja el arco
+ 
+    # Pintamos la semi dona de la izquierda
+    for i in range(len(interior)):
+        vertices.append(interior[i])
+        vertices.append(exterior[i])
+    cara(vertices, (1,1,1)) # pinta la semi dona
+
+    vertices.clear()
+
+    cara([vertice4, vertice3, exterior[0], interior[0]], (1,1,1)) # Pintar lazo de arriba izquierda
+    cara([vertice2, vertice3, exterior[-1], interior[-1]], (1,1,1)) # Pintar lazo de abajo izquierda
+   
+    cara([vertice1, vertice4, vertice3, vertice2, vertice1], (1,1,1)) # Pintar cuadrado central
 
     glFlush() # Para forzar a que pinte.
     # glFinish()
